93791389806a9620f411d7be0ff532e5ca8c6b46
Jonas Weigert <weigert.jonas@gmail.com>
2017-09-25 02:59:58 +0000

    prepare pusher broadcasting

--- public/js/app.js
+++ public/js/app.js
@@ -51981,7 +51981,62 @@ module.exports = __webpack_require__(137);
 
 /***/ }),
 /* 296 */,
-/* 297 */,
+/* 297 */
+/***/ (function(module, exports) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function() {
+	var list = [];
+
+	// return the list of modules as css string
+	list.toString = function toString() {
+		var result = [];
+		for(var i = 0; i < this.length; i++) {
+			var item = this[i];
+			if(item[2]) {
+				result.push("@media " + item[2] + "{" + item[1] + "}");
+			} else {
+				result.push(item[1]);
+			}
+		}
+		return result.join("");
+	};
+
+	// import a list of modules into the list
+	list.i = function(modules, mediaQuery) {
+		if(typeof modules === "string")
+			modules = [[null, modules, ""]];
+		var alreadyImportedModules = {};
+		for(var i = 0; i < this.length; i++) {
+			var id = this[i][0];
+			if(typeof id === "number")
+				alreadyImportedModules[id] = true;
+		}
+		for(i = 0; i < modules.length; i++) {
+			var item = modules[i];
+			// skip already imported module
+			// this implementation is not 100% perfect for weird media query combinations
+			//  when a module is imported multiple times with different media queries.
+			//  I hope this will never occur (Hey this way we have smaller bundles)
+			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+				if(mediaQuery && !item[2]) {
+					item[2] = mediaQuery;
+				} else if(mediaQuery) {
+					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+				}
+				list.push(item);
+			}
+		}
+	};
+	return list;
+};
+
+
+/***/ }),
 /* 298 */,
 /* 299 */,
 /* 300 */,
@@ -52155,13 +52210,92 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+var moment = __webpack_require__(0);
 
 /* harmony default export */ __webpack_exports__["default"] = ({
+    data: function data() {
+        return {
+            applications: null,
+            moment: moment
+        };
+    },
     mounted: function mounted() {
+
+        var vm = this;
+
         console.log('BeanbotDashboard mounted.');
 
         axios.get('/api/dashboard').then(function (response) {
-            console.log(response.data);
+            vm.applications = response.data;
         }).catch(function (error) {
             console.log(error);
         });
@@ -52171,6 +52305,10 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /***/ }),
 /* 395 */
 /***/ (function(module, exports, __webpack_require__) {
+
+
+/* styles */
+__webpack_require__(398)
 
 var Component = __webpack_require__(390)(
   /* script */
@@ -52207,21 +52345,83 @@ module.exports = Component.exports
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
-  return _vm._m(0)
-},staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
   return _c('div', {
     staticClass: "container"
-  }, [_c('div', {
-    staticClass: "row"
-  }, [_c('div', {
-    staticClass: "col-md-8 col-md-offset-2"
-  }, [_c('div', {
+  }, [(_vm.applications === null) ? _c('div', {
+    staticClass: "panel"
+  }, [_vm._m(0)]) : _c('div', {
     staticClass: "panel panel-default"
-  }, [_c('div', {
-    staticClass: "panel-heading"
-  }, [_vm._v("Example Component")]), _vm._v(" "), _c('div', {
-    staticClass: "panel-body"
-  }, [_vm._v("\n                    I'm an example component!\n                ")])])])])])
+  }, [_c('table', {
+    staticClass: "table table-hover table-eb"
+  }, _vm._l((_vm.applications), function(environments, application) {
+    return _c('tbody', [_c('tr', [_vm._m(1, true), _vm._v(" "), _c('th', {
+      attrs: {
+        "colspan": "2"
+      }
+    }, [_vm._v(_vm._s(application))]), _vm._v(" "), _c('th', [_vm._v("Status")]), _vm._v(" "), _c('th', [_vm._v("Deploy")]), _vm._v(" "), _c('th')]), _vm._v(" "), _vm._l((environments), function(environment) {
+      return _c('tr', {
+        class: {
+          'danger': environment.status.status === 'Severe' || environment.status.status === 'Degraded',
+            'warning': environment.status.status === 'Warning'
+        }
+      }, [_c('td', {
+        attrs: {
+          "width": "1"
+        }
+      }), _vm._v(" "), _c('td', {
+        attrs: {
+          "width": "1"
+        }
+      }, [_c('i', {
+        staticClass: "fa fa-circle fa-1x",
+        class: {
+          'status-ok': environment.status.status === 'Ok',
+            'status-info': environment.status.status === 'Info',
+            'status-warning': environment.status.status === 'Warning',
+            'status-unknown': environment.status.status === 'Unknown',
+            'status-degraded': environment.status.status === 'Degraded',
+            'status-severe': environment.status.status === 'Severe'
+        }
+      })]), _vm._v(" "), _c('td', [_c('a', {
+        attrs: {
+          "href": "https://console.aws.amazon.com/elasticbeanstalk/home",
+          "target": "_blank"
+        }
+      }, [_vm._v(_vm._s(environment.eb_environment))])]), _vm._v(" "), _c('td', [(environment.status !== null) ? _c('span', [_c('strong', {
+        staticStyle: {
+          "font-family": "monospace",
+          "text-transform": "uppercase"
+        }
+      }, [_vm._v(_vm._s(environment.status.status))]), _c('br'), _vm._v(" "), _c('small', [_vm._v(_vm._s(_vm.moment.utc().to(_vm.moment.utc(environment.status.status_set_at))))])]) : _c('span', [_vm._m(2, true)])]), _vm._v(" "), _c('td', [(environment.last_deployment !== null) ? _c('span', [(environment.last_deployment.deployment_completed_at == null) ? _c('span', [_c('i', {
+        staticClass: "fa fa-refresh fa-spin"
+      }), _vm._v("\n                                Deploying...\n                            ")]) : _c('span', [_vm._v("\n                                " + _vm._s(_vm.moment.utc(environment.last_deployment.deployment_completed_at).format('ddd, MMM Do YYYY, h:mm A')) + " UTC"), _c('br'), _vm._v(" "), _c('small', [_vm._v("\n                                    " + _vm._s(_vm.moment.utc().to(_vm.moment.utc(environment.last_deployment.created_at))) + ",\n                                    Duration: " + _vm._s(_vm.moment(environment.last_deployment.created_at).to(environment.last_deployment.deployment_completed_at, true)) + "\n                                ")])])]) : _c('span', [_vm._m(3, true)])]), _vm._v(" "), _c('td', {
+        attrs: {
+          "width": "1"
+        }
+      }, [_c('a', {
+        staticClass: "btn btn-default",
+        attrs: {
+          "href": '/eb-environments/' + environment.id
+        }
+      }, [_c('i', {
+        staticClass: "fa fa-arrow-right"
+      })])])])
+    })], 2)
+  }))])])
+},staticRenderFns: [function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('div', {
+    staticClass: "panel-body text-center"
+  }, [_c('br'), _vm._v(" "), _c('p', [_c('i', {
+    staticClass: "fa fa-circle-o-notch fa-spin fa-2x"
+  })]), _vm._v(" "), _c('small', [_vm._v("Fetching Elastic Beanstalk Status")])])
+},function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('th', [_c('i', {
+    staticClass: "fa fa-globe fa-2x"
+  })])
+},function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('em', [_vm._v("\n                                Unknown"), _c('br'), _vm._v(" "), _c('small', [_vm._v("Never Reported")])])
+},function (){var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;
+  return _c('em', [_vm._v("\n                                Unknown"), _c('br'), _vm._v(" "), _c('small', [_vm._v("No Deploy Detected Yet")])])
 }]}
 module.exports.render._withStripped = true
 if (false) {
@@ -52230,6 +52430,293 @@ if (false) {
      require("vue-hot-reload-api").rerender("data-v-9363faca", module.exports)
   }
 }
+
+/***/ }),
+/* 397 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(297)();
+exports.push([module.i, "\ntable.table-eb > tbody > tr > td,\ntable.table-eb > tbody > tr > th {\n    vertical-align:middle;\n}\n\n", ""]);
+
+/***/ }),
+/* 398 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(397);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(399)("77dd76ad", content, false);
+// Hot Module Replacement
+if(false) {
+ // When the styles change, update the <style> tags
+ if(!content.locals) {
+   module.hot.accept("!!../../../../node_modules/css-loader/index.js!../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"id\":\"data-v-9363faca\",\"scoped\":false,\"hasInlineConfig\":true}!../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./BeanbotDashboard.vue", function() {
+     var newContent = require("!!../../../../node_modules/css-loader/index.js!../../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"id\":\"data-v-9363faca\",\"scoped\":false,\"hasInlineConfig\":true}!../../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./BeanbotDashboard.vue");
+     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+     update(newContent);
+   });
+ }
+ // When the module is disposed, remove the <style> tags
+ module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 399 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+  Modified by Evan You @yyx990803
+*/
+
+var hasDocument = typeof document !== 'undefined'
+
+if (typeof DEBUG !== 'undefined' && DEBUG) {
+  if (!hasDocument) {
+    throw new Error(
+    'vue-style-loader cannot be used in a non-browser environment. ' +
+    "Use { target: 'node' } in your Webpack config to indicate a server-rendering environment."
+  ) }
+}
+
+var listToStyles = __webpack_require__(400)
+
+/*
+type StyleObject = {
+  id: number;
+  parts: Array<StyleObjectPart>
+}
+
+type StyleObjectPart = {
+  css: string;
+  media: string;
+  sourceMap: ?string
+}
+*/
+
+var stylesInDom = {/*
+  [id: number]: {
+    id: number,
+    refs: number,
+    parts: Array<(obj?: StyleObjectPart) => void>
+  }
+*/}
+
+var head = hasDocument && (document.head || document.getElementsByTagName('head')[0])
+var singletonElement = null
+var singletonCounter = 0
+var isProduction = false
+var noop = function () {}
+
+// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+// tags it will allow on a page
+var isOldIE = typeof navigator !== 'undefined' && /msie [6-9]\b/.test(navigator.userAgent.toLowerCase())
+
+module.exports = function (parentId, list, _isProduction) {
+  isProduction = _isProduction
+
+  var styles = listToStyles(parentId, list)
+  addStylesToDom(styles)
+
+  return function update (newList) {
+    var mayRemove = []
+    for (var i = 0; i < styles.length; i++) {
+      var item = styles[i]
+      var domStyle = stylesInDom[item.id]
+      domStyle.refs--
+      mayRemove.push(domStyle)
+    }
+    if (newList) {
+      styles = listToStyles(parentId, newList)
+      addStylesToDom(styles)
+    } else {
+      styles = []
+    }
+    for (var i = 0; i < mayRemove.length; i++) {
+      var domStyle = mayRemove[i]
+      if (domStyle.refs === 0) {
+        for (var j = 0; j < domStyle.parts.length; j++) {
+          domStyle.parts[j]()
+        }
+        delete stylesInDom[domStyle.id]
+      }
+    }
+  }
+}
+
+function addStylesToDom (styles /* Array<StyleObject> */) {
+  for (var i = 0; i < styles.length; i++) {
+    var item = styles[i]
+    var domStyle = stylesInDom[item.id]
+    if (domStyle) {
+      domStyle.refs++
+      for (var j = 0; j < domStyle.parts.length; j++) {
+        domStyle.parts[j](item.parts[j])
+      }
+      for (; j < item.parts.length; j++) {
+        domStyle.parts.push(addStyle(item.parts[j]))
+      }
+      if (domStyle.parts.length > item.parts.length) {
+        domStyle.parts.length = item.parts.length
+      }
+    } else {
+      var parts = []
+      for (var j = 0; j < item.parts.length; j++) {
+        parts.push(addStyle(item.parts[j]))
+      }
+      stylesInDom[item.id] = { id: item.id, refs: 1, parts: parts }
+    }
+  }
+}
+
+function createStyleElement () {
+  var styleElement = document.createElement('style')
+  styleElement.type = 'text/css'
+  head.appendChild(styleElement)
+  return styleElement
+}
+
+function addStyle (obj /* StyleObjectPart */) {
+  var update, remove
+  var styleElement = document.querySelector('style[data-vue-ssr-id~="' + obj.id + '"]')
+
+  if (styleElement) {
+    if (isProduction) {
+      // has SSR styles and in production mode.
+      // simply do nothing.
+      return noop
+    } else {
+      // has SSR styles but in dev mode.
+      // for some reason Chrome can't handle source map in server-rendered
+      // style tags - source maps in <style> only works if the style tag is
+      // created and inserted dynamically. So we remove the server rendered
+      // styles and inject new ones.
+      styleElement.parentNode.removeChild(styleElement)
+    }
+  }
+
+  if (isOldIE) {
+    // use singleton mode for IE9.
+    var styleIndex = singletonCounter++
+    styleElement = singletonElement || (singletonElement = createStyleElement())
+    update = applyToSingletonTag.bind(null, styleElement, styleIndex, false)
+    remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true)
+  } else {
+    // use multi-style-tag mode in all other cases
+    styleElement = createStyleElement()
+    update = applyToTag.bind(null, styleElement)
+    remove = function () {
+      styleElement.parentNode.removeChild(styleElement)
+    }
+  }
+
+  update(obj)
+
+  return function updateStyle (newObj /* StyleObjectPart */) {
+    if (newObj) {
+      if (newObj.css === obj.css &&
+          newObj.media === obj.media &&
+          newObj.sourceMap === obj.sourceMap) {
+        return
+      }
+      update(obj = newObj)
+    } else {
+      remove()
+    }
+  }
+}
+
+var replaceText = (function () {
+  var textStore = []
+
+  return function (index, replacement) {
+    textStore[index] = replacement
+    return textStore.filter(Boolean).join('\n')
+  }
+})()
+
+function applyToSingletonTag (styleElement, index, remove, obj) {
+  var css = remove ? '' : obj.css
+
+  if (styleElement.styleSheet) {
+    styleElement.styleSheet.cssText = replaceText(index, css)
+  } else {
+    var cssNode = document.createTextNode(css)
+    var childNodes = styleElement.childNodes
+    if (childNodes[index]) styleElement.removeChild(childNodes[index])
+    if (childNodes.length) {
+      styleElement.insertBefore(cssNode, childNodes[index])
+    } else {
+      styleElement.appendChild(cssNode)
+    }
+  }
+}
+
+function applyToTag (styleElement, obj) {
+  var css = obj.css
+  var media = obj.media
+  var sourceMap = obj.sourceMap
+
+  if (media) {
+    styleElement.setAttribute('media', media)
+  }
+
+  if (sourceMap) {
+    // https://developer.chrome.com/devtools/docs/javascript-debugging
+    // this makes source maps inside style tags work properly in Chrome
+    css += '\n/*# sourceURL=' + sourceMap.sources[0] + ' */'
+    // http://stackoverflow.com/a/26603875
+    css += '\n/*# sourceMappingURL=data:application/json;base64,' + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + ' */'
+  }
+
+  if (styleElement.styleSheet) {
+    styleElement.styleSheet.cssText = css
+  } else {
+    while (styleElement.firstChild) {
+      styleElement.removeChild(styleElement.firstChild)
+    }
+    styleElement.appendChild(document.createTextNode(css))
+  }
+}
+
+
+/***/ }),
+/* 400 */
+/***/ (function(module, exports) {
+
+/**
+ * Translates the list format produced by css-loader into something
+ * easier to manipulate.
+ */
+module.exports = function listToStyles (parentId, list) {
+  var styles = []
+  var newStyles = {}
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i]
+    var id = item[0]
+    var css = item[1]
+    var media = item[2]
+    var sourceMap = item[3]
+    var part = {
+      id: parentId + ':' + i,
+      css: css,
+      media: media,
+      sourceMap: sourceMap
+    }
+    if (!newStyles[id]) {
+      styles.push(newStyles[id] = { id: id, parts: [part] })
+    } else {
+      newStyles[id].parts.push(part)
+    }
+  }
+  return styles
+}
+
 
 /***/ })
 /******/ ]);
--- resources/assets/js/components/BeanbotDashboard.vue
+++ resources/assets/js/components/BeanbotDashboard.vue
@@ -1,27 +1,108 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
+        
+        <div class="panel" v-if="applications === null">
+            <div class="panel-body text-center">
+                <br />
+                <p><i class="fa fa-circle-o-notch fa-spin fa-2x"></i></p>
+                <small>Fetching Elastic Beanstalk Status</small>
             </div>
+        </div>
+        
+        <div class="panel panel-default" v-else="applications === null">
+            <table class="table table-hover table-eb">
+                <tbody v-for="(environments, application) in applications">
+                    <tr>
+                        <th><i class="fa fa-globe fa-2x"></i></th>
+                        <th colspan=2>{{ application }}</th>
+                        <th>Status</th>
+                        <th>Deploy</th>
+                        <th></th>
+                    </tr>
+                    <tr v-for="environment in environments" v-bind:class="{ 
+                        'danger': environment.status.status === 'Severe' ||  environment.status.status === 'Degraded', 
+                        'warning': environment.status.status === 'Warning'}">
+                        <td width="1"></td>
+                        <td width="1">
+                            <i class="fa fa-circle fa-1x" v-bind:class="{ 
+                                'status-ok': environment.status.status === 'Ok',
+                                'status-info': environment.status.status === 'Info',
+                                'status-warning': environment.status.status === 'Warning',
+                                'status-unknown': environment.status.status === 'Unknown',
+                                'status-degraded': environment.status.status === 'Degraded',
+                                'status-severe': environment.status.status === 'Severe'
+                                }"></i>
+                        </td>
+                        <td>
+                            <a href="https://console.aws.amazon.com/elasticbeanstalk/home" target="_blank">{{ environment.eb_environment }}</a>
+                        </td>
+                        <td>
+                            <span v-if="environment.status !== null">
+                                <strong style="font-family:monospace; text-transform:uppercase;">{{ environment.status.status }}</strong><br />
+                                <small>{{ moment.utc().to(moment.utc(environment.status.status_set_at)) }}</small>
+                            </span>
+                            <span v-else="environment.status !== null">
+                                <em>
+                                    Unknown<br />
+                                    <small>Never Reported</small>
+                                </em>
+                            </span>
+                        </td>
+                        <td>
+                            <span v-if="environment.last_deployment !== null">
+                                <span v-if="environment.last_deployment.deployment_completed_at == null">
+                                    <i class="fa fa-refresh fa-spin"></i>
+                                    Deploying...
+                                </span>
+                                <span v-else="environment.last_deployment.deployment_completed_at == null">
+                                    {{ moment.utc(environment.last_deployment.deployment_completed_at).format('ddd, MMM Do YYYY, h:mm A') }} UTC<br />
+                                    <small>
+                                        {{ moment.utc().to(moment.utc(environment.last_deployment.created_at)) }},
+                                        Duration: {{ moment(environment.last_deployment.created_at).to(environment.last_deployment.deployment_completed_at, true) }}
+                                    </small>
+                                </span>
+                            </span>
+                            <span v-else="environment.last_deployment !== null">
+                                <em>
+                                    Unknown<br />
+                                    <small>No Deploy Detected Yet</small>
+                                </em>
+                            </span>
+                        </td>
+                        <td width="1">
+                            <a :href="'/eb-environments/'+ environment.id" class="btn btn-default">
+                                <i class="fa fa-arrow-right"></i>
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script>
+
+    var moment = require('moment');
+
     export default {
+        
+        data() {
+            return {
+                applications: null,
+                moment: moment,
+            }  
+        },
+        
         mounted() {
+            
+            var vm = this;
+            
             console.log('BeanbotDashboard mounted.')
             
             axios.get('/api/dashboard')
                 .then(function(response) {
-                    console.log(response.data);
+                    vm.applications = response.data;
                 })
                 .catch(function(error) {
                     console.log(error);
@@ -29,3 +110,12 @@
         }
     }
 </script>
+
+<style type="text/css">
+    
+    table.table-eb > tbody > tr > td,
+    table.table-eb > tbody > tr > th {
+        vertical-align:middle;
+    }
+    
+</style>
