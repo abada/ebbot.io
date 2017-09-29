94fb05404d3b6208ac4c72220b86aa6985ddde24
Jonas Weigert <weigert.jonas@gmail.com>
2017-09-29 02:40:00 +0000

    favicon

--- resources/views/vendor/spark/layouts/app-nonav.blade.php
+++ resources/views/vendor/spark/layouts/app-nonav.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="favicon" href="/img/beanbot.png">
+    
     <title>@yield('title', config('app.name'))</title>
 
     <!-- Fonts -->
