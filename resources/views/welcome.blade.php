9b34ebb300e2eff9fe87811358b26d80e18c8d84
Jonas Weigert <weigert.jonas@gmail.com>
2017-09-29 01:27:11 +0000

    copy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                     </li>
                     <li>
                         <strong>Insights:</strong>
-                        <small>Deploy Runtime / Deploy Rate / Status Reporting</small>
+                        <small>Deploy Runtime / Deploy Rate / Status Change Logs</small>
                     </li>
                 </ul>
                 
