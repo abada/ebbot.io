c476d5a0781cc1fa2d2a4f32cb855c6af951e467
Jonas Weigert <weigert.jonas@gmail.com>
2017-10-09 00:47:24 +0000

    implemented repo method for deployment duration projection

--- app/Repositories/DeploymentRepository.php
+++ app/Repositories/DeploymentRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\EbEnvironment;
+use Oefenweb\Statistics\Statistics;
 
 class DeploymentRepository 
 {
@@ -10,6 +11,17 @@ class DeploymentRepository
     public function getLastDeploymentForEbEnvironment(EbEnvironment $env)
     {
         return $env->deployments()->latest('created_at')->first();
+    }
+    
+    public function getProjectedDeploymentDurationForEbEnvironment(EbEnvironment $env)
+    {
+        $durations = $env->deployments()->orderBy('id', 'DESC')->limit(5)->pluck('duration')->toArray();
+        
+        // DETERMINE AVG & STDDEV
+        $duration_avg = Statistics::mean($durations);
+        $duration_stddev = Statistics::standardDeviation($durations);
+        
+        return $duration_avg + $duration_stddev;
     }
     
 }
