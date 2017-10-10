8183a357ec0eace7d6bfaf319fe796d8ff71efd0
Jonas Weigert <weigert.jonas@gmail.com>
2017-10-10 11:15:54 +0000

    change deployment duration projection by using avg of durations & excluding outliers of 1 stddev

--- app/Repositories/DeploymentRepository.php
+++ app/Repositories/DeploymentRepository.php
@@ -20,17 +20,26 @@ class DeploymentRepository
         if(!is_null($before)) {
             $query = $query->where('created_at', '<', $before);
         }
-        $durations = $query->limit(5)->pluck('duration')->toArray();
+        $durations = $query->limit(10)->pluck('duration')->toArray();
         if(count($durations) < 2) {
             return null;
         }
         
         // DETERMINE AVG & STDDEV
         $duration_avg = Statistics::mean($durations);
-        //$duration_stddev = Statistics::standardDeviation($durations);
+        $duration_stddev = Statistics::standardDeviation($durations);
         
-        //return $duration_avg + $duration_stddev;
-        return $duration_avg;
+        // ELIMINATE OUTLIERS
+        $durations_without_outliers = [];
+        $limit_upper = $duration_avg + $duration_stddev;
+        $limit_lower = $duration_avg - $duration_stddev;
+        foreach($durations as $duration) {
+            if($duration <  $limit_upper && $duration > $limit_lower) {
+                $durations_without_outliers[$duration];
+            }
+        }
+    
+        return Statistics::mean($durations_without_outliers);
     }
     
 }
